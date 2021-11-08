00a9e83a20fe3a8ec79af65fd1a8a036800da7ee
Pulkit Gupta <pulkit.g@students.iiit.ac.in>
2021-11-09 03:01:08 +0530

    Added Lineplot

--- dashboard/frontend/src/App.js
+++ dashboard/frontend/src/App.js
@@ -15,6 +15,7 @@ function App() {
       <Route path="/login" exact component={Login}/>
       <Route path="/register" exact component={Register}/>
       <Route path="/dashboard" exact component={Dashboard}/>
+      <Route path="/line_plot" exact component={Lineplot}/>
       </Switch>
     </div>
     </Router>
