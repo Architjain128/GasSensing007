f13c535ce3687dc61ceda5eca56d91e211b28869
Aryan Jain <56213387+Rockarya@users.noreply.github.com>
2021-12-08 18:07:22 +0530

    Create temp.ino

---
+++ gas_sensor_main/temp.ino
@@ -0,0 +1,26 @@
+void sendMail(int reading) {
+  ESP_Mail_Session session;
+  session.server.host_name = SMTP_HOST;
+  session.server.port = SMTP_PORT;
+  session.login.email = AUTHOR_EMAIL;
+  session.login.password = AUTHOR_PASSWORD;
+  session.login.user_domain = "";
+  SMTP_Message message;
+
+  /* Set the message headers */
+  message.sender.name = "ESP";
+  message.sender.email = AUTHOR_EMAIL;
+  message.subject = "ESP Test Email";
+  message.addRecipient("Pulkit", RECIPIENT_EMAIL);
+
+  String htmlMsg = "<div style=\"color:#2f4468;\"><h1>Danger! LPG Gas levels above threshold." + String(reading) + " is the current value.</h1><p>- Sent from ESP board</p></div>";
+  message.html.content = htmlMsg.c_str();
+  message.html.content = htmlMsg.c_str();
+  message.text.charSet = "us-ascii";
+  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+
+  if (!smtp.connect(&session))
+    return;
+  if (!MailClient.sendMail(&smtp, &message))
+    Serial.println("Error sending Email, " + smtp.errorReason());
+}
