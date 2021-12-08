429e4a33ca826cb7ed5ae5c3aceb822c1711aaec
Aryan Jain <56213387+Rockarya@users.noreply.github.com>
2021-12-08 17:05:37 +0530

    Delete temp.ino

--- gas_sensor_main/temp.ino
+++
@@ -1,7 +0,0 @@
- int httpResponseCode = http.POST("{\"m2m:cin\":{\"lbl\":[ \"Label-1\",\"Label-2\"],\"con\": \"[7  ," + date + " " + timeStamp + ", " +tempStr + "]\"}}");
-  if(httpResponseCode>0){
-    String response = http.getString();                       
-    Serial.println(httpResponseCode);   
-    Serial.println(response);
-  }
-}
