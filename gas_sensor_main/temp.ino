03a9a1e28394784d07da4daa58f38bbe355a66fb
Aryan Jain <56213387+Rockarya@users.noreply.github.com>
2021-12-08 16:15:32 +0530

    Update temp.ino

--- gas_sensor_main/temp.ino
+++ gas_sensor_main/temp.ino
@@ -1,62 +1,86 @@
-void sendDataToOneM2M(int reading, bool aboveT) {
-  HTTPClient http;
-  // Your Domain name with URL path or IP address with path
-  http.begin(serverName);
-  // Specify content-type header
-  http.addHeader("X-M2M-Origin", "fRZvzNA7Bp:i43Yn0WPem");
-  http.addHeader("Content-Type", "application/json;ty=4");
-  http.addHeader("Content-Length", "500");
-  http.addHeader("Connection", "close");
-  
-  DateTime now = rtc.now();
-  int sec = now.second(), minut = now.minute(), hr = now.hour(), mont = now.month(), dayy = now.day();
-  String Sec = String(sec), Min = String(minut), Hr = String(hr), Month = String(mont), Day = String(dayy);
-  if(sec < 10){
-    Sec = "0" + Sec;
-  }
-  if(minut < 10) {
-    Min = "0" + Min;
-  }
-  if(hr < 10) {
-    Hr = "0" + Hr;
-  }
-  if(mont < 10) {
-    Month = "0" + Month;
-  }
-  if(dayy < 10) {
-    Day = "0" + Day;
-  }
-  String timeStamp = Hr + ":" + Min + ":" + Sec;
-  String date = String(now.year()) + '/' + Month + '/' + Day;
+async function aaa(){
+            await axios.get("https://middlecors.herokuapp.com/https://esw-onem2m.iiit.ac.in/~/in-cse/in-name/Team-15/Node-1/Data/?rcn=4",{
+                headers : {
+                        "X-M2M-Origin": "fRZvzNA7Bp:i43Yn0WPem",
+                    }
+            })
+            .then(function (res) {
+                // console.log(res.data);
+                let rows = []
+                xml2js.parseString(res.data, (err, result) => {
+                    if(err) {
+                        throw err;
+                    }
+                    const json = JSON.stringify(result, null, 4);
+                    const obj = JSON.parse(json);
+                    var json_data = [] , temp_data = []
+                    temp_data = obj['m2m:cnt']['m2m:cin']
+                    for(var i=0;i<temp_data.length;i++)
+                    {
+                        json_data.push(temp_data[i]['con'])
+                    }
+                    var c = 0
+                    for(var i=0;i<json_data.length;i++)
+                    {
+                        var str = json_data[i][0]
+                        var f = 0
+                        for(var j=0;j<str.length;j++)
+                        {
+                            if(str[j]==',')
+                            {
+                                f=1
+                                break
+                            }
+                        }
+                        if(f==1)
+                        {
+                            c+=1
+                            f = 0
+                            var time="",reading=""
+                            if(str[1]==="1" && str[2]==="0"){   // data channel 6 for old encryption 8 for encryption testing
+                                for(var j=5;j<str.length-1;j++)
+                                {
+                                    if(str[j]==',')
+                                    {
+                                        f=1
+                                        continue
+                                    }
+                                    if(f==0)
+                                    {
+                                        time += str[j]
+                                    }
+                                    else
+                                    {
+                                        reading += str[j]
+                                    }
+                                }
+                                let value=reading
+                                // removing whitespaces from front and back
+                                value = value.trim()
 
-  String message = String(reading);
-  String p = "%$6@#2&*()__-+=;,.<>3/~`%$7@#^&8()55_-+9";
-  int len = message.length();
-  char ch;
-  String encrypted_mes = "";
-  for(int i = 0; i<len; ++i){
-    ch = message[i];
-        encrypted_mes += ch + p;      
-  }
+                                // decryption
+                                var aesEcb = require('aes-ecb');
+                                var encrypt = Buffer.from(value, 'hex').toString('base64')
+                                var decrypt = aesEcb.decrypt(hash_key, encrypt);
+                                
+                                var true_reading = ''
+                                for(let i=0;i<7;i++)
+                                {
+                                    true_reading+=decrypt[i]
+                                }
 
-  mbedtls_aes_context aes;
-  char *key = "zyqonqidsgqfshzz";
-  char *input_text  ="2599.69";
-//  unsigned char decrypted_text_output[16];
-  unsigned char encrypted_text_output[16];
-  encrypt(input_text, key, encrypted_text_output);
-  String tempStr;
-
-  for (int i = 0; i < 16; i++) {
-    char str[1];
-    sprintf(str, "%02x", (int)encrypted_text_output[i]);
-    tempStr += str;
-  }
-  
-  int httpResponseCode = http.POST("{\"m2m:cin\":{\"lbl\":[ \"Label-1\",\"Label-2\"],\"con\": \"[7  ," + date + " " + timeStamp + ", " +tempStr + "]\"}}");
-  if(httpResponseCode>0){
-    String response = http.getString();                       
-    Serial.println(httpResponseCode);   
-    Serial.println(response);
-  }
-}
+                                if(true_reading>'3000')
+                                rows.push({ id: c, index: c, timestamp: time, reading: true_reading, status: "☠️"})
+                            }
+                        }
+                    }
+                });
+                
+                roww = rows.reverse()
+                return rows
+            })
+            .catch(function (error) {
+                console.log(error);
+                return -1
+            });
+        }
