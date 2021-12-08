8c59a717af352276b5010ab1937c14c91b63f5cf
Aryan Jain <56213387+Rockarya@users.noreply.github.com>
2021-12-08 18:18:08 +0530

    Delete temp.ino

--- gas_sensor_main/temp.ino
+++
@@ -1,8 +0,0 @@
-hash_key = localStorage.getItem('gas-encrypt-key')
-let roww = []
-async function aaa(){
-    await axios.get("https://middlecors.herokuapp.com/https://esw-onem2m.iiit.ac.in/~/in-cse/in-name/Team-15/Node-1/Data/?rcn=4",{
-        headers : {
-                "X-M2M-Origin": "fRZvzNA7Bp:i43Yn0WPem",
-            }
-    })
