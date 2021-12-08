1bf1b0c81cbb3145f34049fc2a03fbe228526ce0
Aryan Jain <56213387+Rockarya@users.noreply.github.com>
2021-12-08 18:13:30 +0530

    Update temp.ino

--- gas_sensor_main/temp.ino
+++ gas_sensor_main/temp.ino
@@ -1,26 +1,8 @@
-void sendMail(int reading) {
-  ESP_Mail_Session session;
-  session.server.host_name = SMTP_HOST;
-  session.server.port = SMTP_PORT;
-  session.login.email = AUTHOR_EMAIL;
-  session.login.password = AUTHOR_PASSWORD;
-  session.login.user_domain = "";
-  SMTP_Message message;
-
-  /* Set the message headers */
-  message.sender.name = "ESP";
-  message.sender.email = AUTHOR_EMAIL;
-  message.subject = "ESP Test Email";
-  message.addRecipient("Pulkit", RECIPIENT_EMAIL);
-
-  String htmlMsg = "<div style=\"color:#2f4468;\"><h1>Danger! LPG Gas levels above threshold." + String(reading) + " is the current value.</h1><p>- Sent from ESP board</p></div>";
-  message.html.content = htmlMsg.c_str();
-  message.html.content = htmlMsg.c_str();
-  message.text.charSet = "us-ascii";
-  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
-
-  if (!smtp.connect(&session))
-    return;
-  if (!MailClient.sendMail(&smtp, &message))
-    Serial.println("Error sending Email, " + smtp.errorReason());
-}
+hash_key = localStorage.getItem('gas-encrypt-key')
+let roww = []
+async function aaa(){
+    await axios.get("https://middlecors.herokuapp.com/https://esw-onem2m.iiit.ac.in/~/in-cse/in-name/Team-15/Node-1/Data/?rcn=4",{
+        headers : {
+                "X-M2M-Origin": "fRZvzNA7Bp:i43Yn0WPem",
+            }
+    })
